3bdc10abd03896882a5138412454ebab3edb2d63
Matheus Brito <matheus.sbrito14@gmail.com>
2025-05-21 14:24:09 -0300

    test: Teste de objetos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,17 @@ package org.example;
 
 import org.example.factoryMethod.Loja;
 import org.example.factoryMethod.LojaEconomica;
+import org.example.factoryMethod.LojaLuxo;
 import org.example.factoryMethod.Veiculo;
 
 public class Main {
     public static void main(String[] args) {
 
-        Loja loja = new LojaEconomica();
-        loja.criarVeiculo("moto").descrever();
+        Loja loja1 = new LojaEconomica();
+        loja1.criarVeiculo("moto").descrever();
+
+        Loja loja2 = new LojaLuxo();
+        loja2.criarVeiculo("carro").descrever();
+
     }
 }
